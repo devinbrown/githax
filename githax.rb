1f7576dd6e54fa73c7acfe71206cd75b016082eb
Devin Brown <devin.g.brown@gmail.com>
2013-03-17 14:06:20 -0600

    SCSS requirements

--- githax.rb
+++ githax.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
 require 'slim'
+require 'sass'
 
 class GitHax < Sinatra::Base
+
+  SCSS_PATH = File.join '..', 'public', 'css'
+
   get '/' do
     slim :index
+  end
+
+  get '/css/main.css' do
+    scss :"#{File.join SCSS_PATH, "main"}"
   end
 end
